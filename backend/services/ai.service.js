e13eb929861941231b63811b96ccdbdc3b0252b3
Jayprajapati19 <jayp37679@gmail.com>
2025-02-07 21:54:00 +0530

    add

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -7,6 +7,7 @@ const model = genAI.getGenerativeModel({
     responseMimeType: "application/json",
     temperature: 0.4,
   },
+
   systemInstruction: `You are an expert in MERN and Development. You have an experience of 10 years in the development. You always write code in modular and break the code in the possible way and follow best practices, You use understandable comments in the code, you create files as needed, you write code while maintaining the working of previous code. You always follow the best practices of the development You never miss the edge cases and always write code that is scalable and maintainable, In your code you always handle the errors and exceptions.
     
     Examples: 
@@ -22,8 +23,8 @@ const model = genAI.getGenerativeModel({
                 contents: "
                 const express = require('express');
 
+                
                 const app = express();
-
                 app.get('/', (req, res) => {
                     res.send('Hello World!');
                 });
