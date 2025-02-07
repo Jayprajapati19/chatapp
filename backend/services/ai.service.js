7843e8e5a570bc74872485f9db5b0076b6c8345c
Jayprajapati19 <jayp37679@gmail.com>
2025-02-07 20:41:34 +0530

    refactor AI service model configuration for improved readability and maintainability

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -1,19 +1,18 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"
-
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-    generationConfig: {
-        responseMimeType: "application/json",
-        temperature: 0.4,
-    },
-    systemInstruction: `You are an expert in MERN and Development. You have an experience of 10 years in the development. You always write code in modular and break the code in the possible way and follow best practices, You use understandable comments in the code, you create files as needed, you write code while maintaining the working of previous code. You always follow the best practices of the development You never miss the edge cases and always write code that is scalable and maintainable, In your code you always handle the errors and exceptions.
+  model: "gemini-1.5-flash",
+  generationConfig: {
+    responseMimeType: "application/json",
+    temperature: 0.4,
+  },
+  systemInstruction: `You are an expert in MERN and Development. You have an experience of 10 years in the development. You always write code in modular and break the code in the possible way and follow best practices, You use understandable comments in the code, you create files as needed, you write code while maintaining the working of previous code. You always follow the best practices of the development You never miss the edge cases and always write code that is scalable and maintainable, In your code you always handle the errors and exceptions.
     
     Examples: 
 
     <example>
- 
+
     response: {
 
     "text": "this is you fileTree structure of the express server",
@@ -98,12 +97,11 @@ const model = genAI.getGenerativeModel({
  IMPORTANT : don't use file name like routes/index.js
        
        
-    `
+    `,
 });
 
 export const generateResult = async (prompt) => {
+  const result = await model.generateContent(prompt);
 
-    const result = await model.generateContent(prompt);
-
-    return result.response.text()
-}
+  return result.response.text();
+};
